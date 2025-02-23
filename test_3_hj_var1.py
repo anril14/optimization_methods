0f29b117c0b5724fb5835d27faef26d9e2dd5317
Alexey Scherbakov <dlyabsinetolko@gmail.com>
2025-02-23 13:23:10 +0300

    Separated test(old and irrelevant) solutions

--- test_3_hj_var1.py
+++ test_3_hj_var1.py
@@ -5,17 +5,17 @@ import numpy as np
 def f(x1,x2):
     return 4*x1**2 + 5*x2**2 - 3*x1*x2 + 9*x1 - 2*x2 + 5
 
-x1space = np.linspace(-5,5,40)
-x2space = np.linspace(-5,5,40)
-X1, X2 = np.meshgrid(x1space, x2space)
-z = f(X1, X2)
+x1_space = np.linspace(-5,5,40)
+x2_space = np.linspace(-5,5,40)
+x1_grid, x2_grid = np.meshgrid(x1_space, x2_space)
+x3_space = f(x1_grid, x2_grid)
 
 plt.ion()
 fig, ax = plt.subplots(figsize = (10,7))
 
 # "tight" layout - more usable space but some bugs with text
 # fig.set_layout_engine("tight")
-ax.contour(x1space,x2space,z,20,zorder = 1)
+ax.contour(x1_space,x2_space,x3_space,20,zorder = 1)
 
 ax.grid(True)
 
@@ -26,19 +26,6 @@ def hooke_jeeves(i=-1, x1=2.0, x2=3.0, dx1 = 1.0, dx2 = 1.0,
                  is_next_i = True, delta = 2.0, acc_ratio = 2.2, eps=0.001):
     old_point = dict({'x1': x1, 'x2': x2})
     fx0 = f(x1, x2)
-
-    # Checking for delta changes only
-    if is_next_i:
-        i += 1
-    is_next_i = True
-
-    if i == 0:
-        plt.scatter(x1, x2, color='blue', zorder=3, label='Start point')
-        fig.canvas.draw()
-        fig.canvas.flush_events()
-
-        time.sleep(2.0)
-        plt.draw()
 
     def check_x1():
         nonlocal x1, x2
@@ -72,15 +59,9 @@ def hooke_jeeves(i=-1, x1=2.0, x2=3.0, dx1 = 1.0, dx2 = 1.0,
             dx2 /= delta
             print('DELTA X2 DOWN')
 
-        if dx1 > eps and dx2 > eps:
-            if old_point['x1'] != x1 and old_point['x2'] != x2:
-                print('*ACCELERATION STEP ACTIVATED')
-                acc_step()
-            if is_next_i:
-                draw('gray','', 2, True)
-            hooke_jeeves(i, x1, x2, dx1, dx2, is_next_i)
-        else:
-            draw('red','End Point', 3, False)
+        if old_point['x1'] != x1 and old_point['x2'] != x2:
+            print('*ACCELERATION STEP ACTIVATED')
+            acc_step()
 
     def acc_step():
         # var acc_ratio is acceleration step ratio when multiplying
@@ -119,8 +100,27 @@ def hooke_jeeves(i=-1, x1=2.0, x2=3.0, dx1 = 1.0, dx2 = 1.0,
         plt.draw()
         time.sleep(0.1)
 
+    #Delta condition
     if dx1 > eps and dx1 > eps:
+        # Checking for delta changes only
+        if is_next_i:
+            if i == -1:
+                plt.scatter(x1, x2, color='blue', zorder=3, label='Start point')
+                fig.canvas.draw()
+                fig.canvas.flush_events()
+
+                time.sleep(2.0)
+                plt.draw()
+            i += 1
+
+        is_next_i = True
         check_x1()
+
+        if is_next_i:
+            draw('gray','', 2, True)
+        hooke_jeeves(i, x1, x2, dx1, dx2, is_next_i)
+    else:
+        draw('red', 'End Point', 3, False)
 
 hooke_jeeves()
 plt.ioff()
