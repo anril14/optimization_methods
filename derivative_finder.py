b12c57d5125914b1aee4120beabebc709fe989dd
Alexey Scherbakov <dlyabsinetolko@gmail.com>
2025-02-23 13:24:11 +0300

    Added derivative finder

---
+++ derivative_finder.py
@@ -0,0 +1,16 @@
+#Find the derivative
+from sympy import *
+
+x1, x2 = symbols('x1 x2')
+expr = 4*x1**2 + 5*x2**2 - 3*x1*x2 + 9*x1 - 2*x2 + 5
+print("Expression : {}".format(expr))
+
+# Use sympy.Derivative() method
+expr_diff1 = Derivative(expr, x1)
+expr_diff2 = Derivative(expr, x2)
+
+print("Derivative of expression with respect to x1 : {}".format(expr_diff1))
+print("Value of the derivative : {}".format(expr_diff1.doit()))
+
+print("Derivative of expression with respect to x2 : {}".format(expr_diff2))
+print("Value of the derivative : {}".format(expr_diff2.doit()))
