f01ca0f740f356268baa5f9d9771b41c7c17acf7
Alexey Scherbakov <dlyabsinetolko@gmail.com>
2025-02-23 09:15:13 +0300

    Fixed iteration counter & added tags on canvas

--- 3hj_var3.py
+++ 3hj_var3.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def f(x1,x2):
-    return x1 ** 2 + 3 * x2 ** 2 + 3 * x1 * x2 - x1 - 2 * x2 - 1
+    return x1**2 + 3*x2**2 + 3*x1*x2-x1 - 2*x2 - 1
 
 x1space = np.linspace(-5,5,40)
 x2space = np.linspace(-5,5,40)
@@ -11,9 +11,10 @@ X1, X2 = np.meshgrid(x1space, x2space)
 z = f(X1, X2)
 
 plt.ion()
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(figsize = (7,7))
 
-fig.set_layout_engine("tight")
+# "tight" layout - more usable space but some bugs with text
+# fig.set_layout_engine("tight")
 ax.contour(x1space,x2space,z,20,zorder = 1)
 
 ax.grid(True)
@@ -22,25 +23,25 @@ plt.axhline(0, color='black', linewidth=1, zorder=2)
 plt.axvline(0, color='black', linewidth=1, zorder=2)
 
 def hooke_jeeves(i=-1, x1=3.0, x2=3.0, dx1 = 1.0, dx2 = 1.0,
-                 delta = 2.0, acc = 2.2, eps=0.0001):
-    point = dict({'x1': x1, 'x2': x2})
+                 is_next_i = True, delta = 2.0, acc_ratio = 2.2, eps=0.001):
+    old_point = dict({'x1': x1, 'x2': x2})
     fx0 = f(x1, x2)
-    i += 1
+
+    # Checking for delta changes only
+    if is_next_i:
+        i += 1
+    is_next_i = True
+
     if i == 0:
+        plt.scatter(x1, x2, color='blue', zorder=3, label='Start point')
         fig.canvas.draw()
         fig.canvas.flush_events()
 
-        time.sleep(1.0)
-        plt.scatter(x1, x2, color='blue', zorder=3)
-
-        fig.canvas.draw()
-        fig.canvas.flush_events()
-
-        time.sleep(1.0)
+        time.sleep(2.0)
         plt.draw()
 
     def check_x1():
-        nonlocal dx1, dx2, eps, x1, x2
+        nonlocal x1, x2
         print(f'\niteration: {i}\nNOW CHECKING X1: X1 {x1} X2 {x2}'
               f' dX1 {dx1} dX2 {dx2}')
         if f(x1 + dx1, x2) < fx0:
@@ -55,7 +56,7 @@ def hooke_jeeves(i=-1, x1=3.0, x2=3.0, dx1 = 1.0, dx2 = 1.0,
             check_x2()
 
     def check_x2():
-        nonlocal dx1, dx2, eps, x1, x2, point
+        nonlocal dx1, dx2, x1, x2, is_next_i
         print(f'NOW CHECKING X2: X1 {x1} X2 {x2} dX1 {dx1} dX2 {dx2}')
         if f(x1, x2 + dx2) < fx0:
             x2 = x2 + dx2
@@ -64,55 +65,64 @@ def hooke_jeeves(i=-1, x1=3.0, x2=3.0, dx1 = 1.0, dx2 = 1.0,
             x2 = x2 - dx2
             print('X2 DOWN TO:', x2)
         else:
-            if point['x1'] == x1 and dx1 >= dx2:
-                dx1 = dx1 / delta
-            dx2 = dx2 / delta
+            if old_point['x1'] == x1 and dx1 >= dx2:
+                print('DELTA X1 DOWN')
+                dx1 /= delta
+                is_next_i = False
+            dx2 /= delta
+            print('DELTA X2 DOWN')
 
         if dx1 > eps and dx2 > eps:
-            if point['x1'] != x1 and point['x2'] != x2:
+            if old_point['x1'] != x1 and old_point['x2'] != x2:
                 print('*ACCELERATION STEP ACTIVATED')
                 acc_step()
-            draw('black', 2)
-            hooke_jeeves(i, x1, x2, dx1, dx2)
+            if is_next_i:
+                draw('gray','', 2, True)
+            hooke_jeeves(i, x1, x2, dx1, dx2, is_next_i)
         else:
-            draw('red', 3)
+            draw('red','End Point', 3, False)
 
     def acc_step():
-        nonlocal x1, x2, point
+        # var acc_ratio is acceleration step ratio when multiplying
+        nonlocal x1, x2
         print(f'VALUES BEFORE STEP: X1 {x1} X2 {x2}')
-        if f(x1, x2) > f((x1 - point['x1']) * acc + point['x1'],
-                         (x1 - point['x2']) * acc + point['x2']):
-            x1 = (x1 - point['x1']) * acc + point['x1']
-            x2 = (x2 - point['x2']) * acc + point['x2']
+        if f(x1, x2) > f((x1 - old_point['x1']) * acc_ratio + old_point['x1'],
+                         (x1 - old_point['x2']) * acc_ratio + old_point['x2']):
+            x1 = (x1 - old_point['x1']) * acc_ratio + old_point['x1']
+            x2 = (x2 - old_point['x2']) * acc_ratio + old_point['x2']
             print('BOTH WAS CHANGED')
-        elif f(x1, x2) > f((x1 - point['x1']) * acc + point['x1'], x2):
-            x1 = (x1 - point['x1']) * acc + point['x1']
+        elif f(x1, x2) > f((x1 - old_point['x1']) * acc_ratio + old_point['x1'], x2):
+            x1 = (x1 - old_point['x1']) * acc_ratio + old_point['x1']
             print('X1 WAS CHANGED')
-        elif f(x1, x2) > f(x1, (x1 - point['x2']) * acc + point['x2']):
-            x2 = (x2 - point['x2']) * acc + point['x2']
+        elif f(x1, x2) > f(x1, (x1 - old_point['x2']) * acc_ratio + old_point['x2']):
+            x2 = (x2 - old_point['x2']) * acc_ratio + old_point['x2']
             print('X2 WAS CHANGED')
         print(f'*VALUES AFTER STEP: X1 {x1} X2 {x2}')
 
-    def draw(color: str, order: int):
+    def draw(color, label: str, order: int, is_text: bool):
         print(f'DRAW POINT {x1} {x2}')
 
-        ax.set_xlim(point['x1']-1*dx1*2,point['x1']+1*dx1*2)
-        ax.set_ylim(point['x2']-1*dx2*2,point['x2']+1*dx2*2)
+        # New zoom
+        ax.set_xlim(old_point['x1']-1*dx1*2,old_point['x1']+1*dx1*2)
+        ax.set_ylim(old_point['x2']-1*dx2*2,old_point['x2']+1*dx2*2)
 
+        # For canvas changing
         fig.canvas.draw()
         fig.canvas.flush_events()
 
-        plt.scatter(x1, x2, color=color, zorder=order)
-        plt.plot([x1, point['x1']], [x2, point['x2']],
+        plt.scatter(x1, x2, color=color, zorder=order, label=label)
+        if is_text:
+            plt.text(x1, x2, str(i+1), zorder = order+1)
+        plt.plot([x1, old_point['x1']], [x2, old_point['x2']],
                  color=color, linewidth='.5')
+
         plt.draw()
-        time.sleep(0.2)
+        time.sleep(0.1)
 
     if dx1 > eps and dx1 > eps:
         check_x1()
 
 hooke_jeeves()
 plt.ioff()
+plt.legend()
 plt.show()
-
-
